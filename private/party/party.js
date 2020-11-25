e8ae803855a929ee729ab3fccf611252c9e3d4a0
cornelissi <simon.cornelis@student.uclouvain.be>
2020-11-25 19:31:54 +0100

    👪 Multiplayer party rooms

--- private/party/party.js
+++ private/party/party.js
@@ -1,17 +1,21 @@
 const socket = io();
 const players = [];
 
+const body = document.querySelector("body");
 const playerListDiv = document.getElementById("players-list");
 const nextGameButton = document.getElementById("next-game");
 
-if (nextGameButton) nextGameButton.onclick = askServerForNextGame;
+nextGameButton.onclick = askServerForNextGame;
 
-let username = prompt("What is your username?");
-socket.emit("new-user", username);
+socket.emit("new-user", room, username);
 
 function askServerForNextGame() {
-  socket.emit("next-game");
+  socket.emit("next-game", room);
 }
+
+socket.on("game", (html) => {
+  body.innerHTML = html;
+});
 
 socket.on("message", (message) => {
   alert(message);
@@ -20,6 +24,16 @@ socket.on("message", (message) => {
 socket.on("all-players", (allPlayers) => {
   playerListDiv.innerHTML = "";
   allPlayers.forEach((username) => addUser(username));
+});
+
+socket.on("you-are-now-admin", () => {
+  nextGameButton.hidden = false;
+});
+
+// TODO transfer admin privileges
+socket.on("you-are-not-admin", () => {
+  nextGameButton.hidden = true;
+  alert("You are not admin.");
 });
 
 function addUser(username) {
--- routes/party.js
+++ routes/party.js
@@ -1,34 +1,78 @@
+const { server } = require("../server.js");
 const { Router } = require("express");
 const socketIO = require("socket.io");
-const server = require("../server.js").server;
+const fs = require("fs");
 
-const io = socketIO(server);
 const partyRouter = new Router();
+const io = socketIO(server);
 
-const players = {};
+// TODO implem database interactions
+const rooms = {};
 
-// TODO change params to session isAdmin or other...
-partyRouter.get("/:admin?", (req, res, next) => {
-  if (req.params.admin === "admin") res.render("admin_party.html");
-  else res.render("party.html");
+partyRouter.get("/:room?", (req, res, next) => {
+  if (!req.params.room) return res.redirect("/");
+  req.session.isAdmin = rooms[req.params.room] === undefined;
+  res.render("party.html", {
+    room: req.params.room,
+    isAdmin: req.session.isAdmin,
+    pseudo: req.session.pseudo,
+  });
 });
 
 io.on("connection", (socket) => {
-  socket.on("new-user", (username) => {
-    players[socket.id] = username;
-    io.emit("all-players", Object.values(players));
+  socket.on("new-user", (room, username) => {
+    if (!room || !username) return;
+    if (!rooms[room]) {
+      rooms[room] = { admin: {}, players: {} };
+      rooms[room].admin[socket.id] = username;
+    }
+    socket.join(room);
+    rooms[room].players[socket.id] = username;
+    io.to(room).emit("all-players", Object.values(rooms[room].players));
   });
 
-  socket.on("next-game", () => {
-    console.log("next-game on server");
-    io.emit("message", "next game has started");
+  socket.on("next-game", (room) => {
+    if (getAdmin(room) != socket.id) return socket.emit("you-are-not-admin");
+    console.log("[ROOMS] " + room + ": next game.");
+    io.to(room).emit("message", "Next game started.");
+    fs.readFile("./private/party/game.xml", (error, fileContent) => {
+      if (error) console.error("[ERROR] " + error);
+      io.to(room).emit("game", fileContent.toString());
+    });
   });
 
   socket.on("disconnect", () => {
-    delete players[socket.id];
-    io.emit("all-players", Object.values(players));
+    findUserRooms(socket).forEach((room) => {
+      delete rooms[room].players[socket.id];
+      if (roomIsEmpty(room)) return delete rooms[room];
+      updateAdmin(room, socket.id);
+      socket.to(room).emit("all-players", Object.values(rooms[room].players));
+      io.to(getAdmin(room)).emit("you-are-now-admin");
+    });
   });
 });
+
+function findUserRooms(socket) {
+  return Object.entries(rooms).reduce((players, [player, room]) => {
+    if (room.players[socket.id]) players.push(player);
+    return players;
+  }, []);
+}
+
+function roomIsEmpty(room) {
+  return Object.values(rooms[room].players).length === 0;
+}
+
+function getAdmin(room) {
+  return Object.keys(rooms[room].admin)[0];
+}
+
+function updateAdmin(room, oldPlayer) {
+  if (getAdmin(room) !== oldPlayer) return;
+  const newAdmin = Object.keys(rooms[room].players)[0];
+  rooms[room].admin = {};
+  rooms[room].admin[newAdmin] = rooms[room].players[newAdmin];
+}
 
 module.exports = {
   partyRouter,
