3b90fa3429c5c4dc2df8ffd4252522157a623995
cornelissi <simon.cornelis@student.uclouvain.be>
2020-11-25 18:23:44 +0100

    👪 Multiplayer party (only one)

--- private/party/party.js
+++ private/party/party.js
@@ -1,8 +1,13 @@
 const socket = io();
+const players = [];
 
+const playerListDiv = document.getElementById("players-list");
 const nextGameButton = document.getElementById("next-game");
 
 if (nextGameButton) nextGameButton.onclick = askServerForNextGame;
+
+let username = prompt("What is your username?");
+socket.emit("new-user", username);
 
 function askServerForNextGame() {
   socket.emit("next-game");
@@ -11,3 +16,15 @@ function askServerForNextGame() {
 socket.on("message", (message) => {
   alert(message);
 });
+
+socket.on("all-players", (allPlayers) => {
+  playerListDiv.innerHTML = "";
+  allPlayers.forEach((username) => addUser(username));
+});
+
+function addUser(username) {
+  players.push(username);
+  let user = document.createElement("h3");
+  user.innerText = username;
+  playerListDiv.appendChild(user);
+}
--- routes/party.js
+++ routes/party.js
@@ -5,19 +5,28 @@ const server = require("../server.js").server;
 const io = socketIO(server);
 const partyRouter = new Router();
 
+const players = {};
+
+// TODO change params to session isAdmin or other...
 partyRouter.get("/:admin?", (req, res, next) => {
   if (req.params.admin === "admin") res.render("admin_party.html");
   else res.render("party.html");
 });
 
-partyRouter.get("/admin", (req, res, next) => {
-  res.render("admin_party.html");
-});
-
 io.on("connection", (socket) => {
+  socket.on("new-user", (username) => {
+    players[socket.id] = username;
+    io.emit("all-players", Object.values(players));
+  });
+
   socket.on("next-game", () => {
     console.log("next-game on server");
     io.emit("message", "next game has started");
+  });
+
+  socket.on("disconnect", () => {
+    delete players[socket.id];
+    io.emit("all-players", Object.values(players));
   });
 });
 
