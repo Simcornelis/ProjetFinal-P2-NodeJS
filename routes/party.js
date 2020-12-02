cb1541fdc713553751daa5b7c718e0f95f4dfed5
cornelissi <simon.cornelis@student.uclouvain.be>
2020-12-02 13:04:47 +0100

    👉 Code review : extracted functions to clarify

--- routes/party.js
+++ routes/party.js
@@ -18,28 +18,15 @@ partyRouter.get("/:partyCode?", (req, res, next) => {
 
 io.on("connection", (socket) => {
   const { partiesCollection } = require("../server.js");
+
   socket.on("new-user", (partyCode, username, userID, team) => {
     if (!partyCode || !username) return;
 
     partiesCollection
       .findOne({ partyCode: partyCode })
-      .then((found) => {
-        if (found) return Object.assign(new Party(), found);
-        else return new Party(partyCode);
-      })
-      .then((party) => {
-        if (userID && party.isAlreadyConnected(userID)) {
-          socket.emit("already-connected");
-          throw new Error("User already connected.");
-        }
-        party.connect(socket.id, username, team, userID);
-        socket.join(partyCode);
-        io.to(party.getAdmin().socketID).emit("you-are-now-admin");
-        io.to(partyCode).emit("players-update", party.getPlayers());
-        console.log(party.getTeams());
-        return party;
-      })
-      .then((party) => updatePartyDB(party))
+      .then((found) => assignParty(found, partyCode))
+      .then((party) => addPlayerToParty(party, socket, username, userID, team))
+      .then((partyWithPlayer) => updatePartyDB(partyWithPlayer))
       .catch((error) => console.error("[ERROR] " + error.stack));
   });
 
@@ -47,19 +34,7 @@ io.on("connection", (socket) => {
     if (!partyCode) return;
 
     getPartyDB(partyCode)
-      .then((party) => {
-        if (party.getAdmin().socketID !== socket.id) {
-          socket.emit("you-are-not-admin");
-          throw new Error("You are not admin.");
-        }
-
-        console.log("[PARTY] (" + partyCode + ") next game.");
-        // io.to(partyCode).emit("message", "Next game started.");
-        fs.readFile("./private/party/game.xml", (error, fileContent) => {
-          if (error) throw new Error("Couldn't get next game.");
-          io.to(partyCode).emit("game", fileContent.toString());
-        });
-      })
+      .then((party) => sendNextGame(party, socket))
       .catch((error) => {
         console.error("[ERROR] " + error.stack);
         socket.emit("message", error.message);
@@ -69,15 +44,56 @@ io.on("connection", (socket) => {
   socket.on("disconnect", () => {
     getUserPartiesDB(socket.id)
       .map((party) => Object.assign(new Party(), party))
-      .forEach((party) => {
-        party.disconnect(socket.id);
-        if (party.isEmpty()) return removePartyDB(party);
-        io.to(party.getAdmin().socketID).emit("you-are-now-admin");
-        io.to(party.partyCode).emit("players-update", party.getPlayers());
-        updatePartyDB(party);
-      });
+      .forEach((party) => disconnectPlayer(party, socket));
   });
 });
+
+// New user functions
+
+function assignParty(party, partyCode) {
+  if (party) return Object.assign(new Party(), party);
+  else return new Party(partyCode);
+}
+
+function addPlayerToParty(party, socket, username, userID, team) {
+  if (userID && party.isAlreadyConnected(userID)) {
+    socket.emit("already-connected");
+    throw new Error("User already connected.");
+  }
+
+  party.connect(socket.id, username, team, userID);
+  socket.join(party.partyCode);
+  io.to(party.getAdmin().socketID).emit("you-are-now-admin");
+  io.to(party.partyCode).emit("players-update", party.getPlayers());
+  console.log(party.getTeams()); // REMOVE
+  return party;
+}
+
+// Next game functions
+
+function sendNextGame(party, socket) {
+  if (party.getAdmin().socketID !== socket.id) {
+    socket.emit("you-are-not-admin");
+    throw new Error("You are not admin.");
+  }
+
+  console.log("[PARTY] [" + party.partyCode + "] next game.");
+  // io.to(party.partyCode).emit("message", "Next game started.");
+  fs.readFile("./private/party/game.xml", (error, fileContent) => {
+    if (error) throw new Error("Couldn't get next game.");
+    io.to(party.partyCode).emit("game", fileContent.toString());
+  });
+}
+
+function disconnectPlayer(party, socket) {
+  party.disconnect(socket.id);
+  if (party.isEmpty()) return removePartyDB(party);
+  io.to(party.getAdmin().socketID).emit("you-are-now-admin");
+  io.to(party.partyCode).emit("players-update", party.getPlayers());
+  updatePartyDB(party);
+}
+
+// DB functions
 
 function updatePartyDB(party) {
   const { partiesCollection } = require("../server.js");
@@ -113,6 +129,4 @@ function removePartyDB(party) {
     .catch((error) => console.error("[ERROR] " + error.stack));
 }
 
-module.exports = {
-  partyRouter,
-};
+module.exports = { partyRouter };
