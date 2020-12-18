d21937e04819ddcf7b9fe86958ae77132ed819c8
Corentin <corentin.libert@student.uclouvain.be>
2020-12-18 20:17:51 +0100

    🎲 [AllGames] New HTML/CSS Design. Search & filter functions.

--- routes/games.js
+++ routes/games.js
@@ -53,8 +53,57 @@ gamesRouter.post("/addgame", (req, res, next) => {
 
 gamesRouter.get("/findgames", async (req, res, next) => {
   const minigamesCollection = require("../server.js").minigamesCollection;
-  return res.json(await minigamesCollection.find({}).toArray());
+  let searchQuery = req.query.search_query;
+  let filterQuery = req.query.filter_query;
+  if (!filterQuery && !searchQuery) {
+    return res.json(await minigamesCollection.find({}).toArray());
+  } else {
+    if (filterQuery && !searchQuery) {
+      return getFilteredGames(filterQuery)
+        .then((found) => res.json(found))
+        .catch((err) => res.status(400).json({ Error: err.toString() }));
+    } else if (searchQuery && !filterQuery) {
+      return getSearchedGames(searchQuery)
+        .then((found) => res.json(found))
+        .catch((err) => res.status(400).json({ Error: err.toString() }));
+    } else {
+      return getFilteredSearchedGames(filterQuery, searchQuery)
+        .then((found) => res.json(found))
+        .catch((err) => res.status(400).json({ Error: err.toString() }));
+    }
+  }
 });
+
+function getSearchedGames(searchQuery) {
+  const minigamesCollection = require("../server.js").minigamesCollection;
+  return minigamesCollection
+    .find(
+      { $text: { $search: searchQuery, $language: "en" } },
+      { score: { $meta: "textScore" } }
+    )
+    .sort({ score: { $meta: "textScore" } })
+    .toArray();
+}
+
+function getFilteredGames(filterQuery) {
+  const minigamesCollection = require("../server.js").minigamesCollection;
+  let selectedCategories = filterQuery.split(",");
+  return minigamesCollection
+    .find({ categories: { $all: selectedCategories } })
+    .toArray();
+}
+
+function getFilteredSearchedGames(filterQuery, searchQuery) {
+  const minigamesCollection = require("../server.js").minigamesCollection;
+  let selectedCategories = filterQuery.split(",");
+  return minigamesCollection
+    .find({
+      categories: { $all: selectedCategories },
+      $text: { $search: searchQuery, $language: "en" },
+    })
+    .sort({ score: { $meta: "textScore" } })
+    .toArray();
+}
 
 module.exports = {
   gamesRouter,
